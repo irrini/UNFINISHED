a795719cc4733a0a83fcdfc0c4691f244bbbe0da
irrini <irenepar222@gmail.com>
2025-04-12 18:19:29 +0200

    navbar link fix

--- unfinished-index/src/components/Navbar.jsx
+++ unfinished-index/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
         <nav className='navbar'>
             <ul className='nav-links'>
                 <li><NavLink to="/" className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>EXPLORE</NavLink></li>
-                <li><NavLink to="/index" className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>INDEX</NavLink></li>
+                <li><NavLink to="/indexpage" className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>INDEX</NavLink></li>
                 <li><NavLink to="/map" className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>MAP</NavLink></li>
                 <li><NavLink to="/about" className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>ABOUT</NavLink></li>
                 <li><NavLink to="/submit" className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>SUBMIT</NavLink></li>
